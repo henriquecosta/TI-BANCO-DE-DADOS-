4acbccff2acc2d2cca8150194436cbc7577bfe03
diego-motta <diego.motta@prodaptech.com.br>
2013-05-27 14:04:10 -0300

    Inicio

--- TI Banco de Dados Parte 3/TI Banco de Dados Parte 3/MedicoCadastro.cs
+++ TI Banco de Dados Parte 3/TI Banco de Dados Parte 3/MedicoCadastro.cs
@@ -5,22 +5,15 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TI_Banco_de_Dados_Parte_3
 {
-    public partial class Form1 : Form
+    public partial class MedicoCadastro : Form
     {
-        public Form1()
+        public MedicoCadastro()
         {
             InitializeComponent();
-        }
-
-        private void btnMedico(object sender, EventArgs e)
-        {
-            MedicoCadastro medico = new MedicoCadastro();
-            medico.Show();
         }
     }
 }
